9ffda6e1c97f793002ad71db04bcffb4e308d0a7
meyzaal <almayza17@gmail.com>
2023-01-04 11:51:49 +0700

    setup bloc

---
+++ lib/app.dart
@@ -0,0 +1,50 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
+
+import 'authentication/bloc/authentication_bloc.dart';
+
+class App extends StatelessWidget {
+  const App(
+      {super.key,
+      required this.authenticationRepository,
+      required this.userRepository});
+
+  final AuthenticationRepository authenticationRepository;
+  final UserRepository userRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+            authenticationRepository: authenticationRepository,
+            userRepository: userRepository),
+        child: const AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  const AppView({super.key});
+
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+          appBar: AppBar(
+        title: const Text('Login App'),
+      )),
+    );
+  }
+}
